6da7713718f04f989ced85ca140c8e7f445dac78
Joyist2021 <77634075+Joyist2021@users.noreply.github.com>
2022-02-27 15:48:23 +0800

    Update about_html.h

--- about_html.h
+++ about_html.h
@@ -11,20 +11,20 @@ const char about_html[] PROGMEM = R"=====(
  <body>
   <ul>
    <li><a class="pull-left" href="#">ESP Radio</a></li>
-   <li><a class="pull-left" href="/index.html">Control</a></li>
-   <li><a class="pull-left" href="/config.html">Config</a></li>
-   <li><a class="pull-left active" href="/about.html">About</a></li>
+   <li><a class="pull-left" href="/index.html">控制</a></li>
+   <li><a class="pull-left" href="/config.html">配置</a></li>
+   <li><a class="pull-left active" href="/about.html">关于</a></li>
   </ul>
   <br><br><br>
   <center>
    <h1>** ESP Radio **</h1>
   </center>
-	<p>Esp Radio -- Webradio receiver for ESP8266, 1.8" color display and VS1053 MP3 module.<br>
-	This project is documented at <a target="blank" href="https://github.com/Edzelf/Esp-radio">Github</a>.</p>
+	<p>Esp Radio -- 基于ESP8266的Webradio接收器，1.8英寸彩色显示屏和VS1053 MP3模块。<br>
+	该项目记录在<a target="blank" href="https://github.com/Edzelf/Esp-radio">Github</a>.</p>
 	<p>Author: Ed Smallenburg<br>
-	Webinterface design: <a target="blank" href="http://www.sanderjochems.nl/">Sander Jochems</a><br>
+	页面设计: <a target="blank" href="http://www.sanderjochems.nl/">Sander Jochems</a><br>
 	App (Android): <a target="blank" href="https://play.google.com/store/apps/details?id=com.thunkable.android.sander542jochems.ESP_Radio">Sander Jochems</a><br>
-	Date: January 2017</p>
+	日期: January 2017</p>
   <script type="text/javascript">
     var stylesheet = document.createElement('link') ;
     stylesheet.href = 'radio.css' ;
